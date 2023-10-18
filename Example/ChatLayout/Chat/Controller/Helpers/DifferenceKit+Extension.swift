bee4c37e363b64464b40e3364a19ce0dfcb8a919
Eugene Kazaev <eugene.kazaev@gmail.com>
2023-10-18 13:50:44 +0100

    Minor cleanup

--- Example/ChatLayout/Chat/Controller/Helpers/DifferenceKit+Extension.swift
+++ Example/ChatLayout/Chat/Controller/Helpers/DifferenceKit+Extension.swift
@@ -25,7 +25,6 @@ extension RecyclerScrollView {
             completion: ((Bool) -> Void)? = nil,
             setData: @escaping ([Section]) -> Void
     ) {
-        print("BEGIN")
         var originalData = originalData
         if case .none = window,
            let data = stagedChangeset.last?.data {
@@ -36,7 +35,6 @@ extension RecyclerScrollView {
                 reloadData()
             }
             completion?(false)
-            print("END")
             return
         }
 
@@ -56,12 +54,11 @@ extension RecyclerScrollView {
                     self.reloadData()
                 }
                 completion?(false)
-                print("END")
                 return
             }
 
             setData(changeset.data)
-            print("I:\(changeset.elementInserted) D:\(changeset.elementDeleted) U:\(changeset.elementUpdated) M:\(changeset.elementMoved)")
+//            print("I:\(changeset.elementInserted) D:\(changeset.elementDeleted) U:\(changeset.elementUpdated) M:\(changeset.elementMoved)")
             var modifications: [ModificationActions] = []
             changeset.elementDeleted.forEach({ indexPath in
                 modifications.append(.delete(indexPath.element))
@@ -95,7 +92,6 @@ extension RecyclerScrollView {
         }
 
         dispatchGroup?.notify(queue: .main) {
-            print("END")
             modificationContext.commit()
             completion!(true)
         }
